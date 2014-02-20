52205f424fbe7b9659ae11e50ae9d720a65b31aa
Bluesnowball <bluesnowball1@googlemail.com>
2014-02-20 10:05:40 +0000

    Update testrun.cc
    First version of routine

--- testrun.cc
+++ testrun.cc
@@ -4,10 +4,7 @@ using namespace std;
 #include <robot_link.h>
 #define ROBOT_NUM 10   // The id number (see below)
 robot_link rlink;      // datatype for the robot link
-void straightRun(int speed);
-void reverse(int speed);
-void veer(bool left, int speed);
-void sharpturn(bool left, int speed);
+void straightRun(int speed), reverse(int speed), veer(bool left, int speed), sharpturn(bool left, int speed), stop;
 
 int main ()
 {                          // data from microprocessor
@@ -17,17 +14,15 @@ if (!rlink.initialise (ROBOT_NUM)) { // setup the link
   return -1;               // error, finish
 }
 
-rlink.command (STOP_IF_HIGH, 0x05);
-rlink.command (STOP_SELECT, 3);
-rlink.command (RAMP_TIME, 255);
-rlink.command(BOTH_MOTORS_GO_SAME, 127);
-delay(2000);
-delay(2000);
+straightRun(127);
+delay(3000);
 float stat = rlink.request (STATUS);
 cout<<stat;
-rlink.command(BOTH_MOTORS_GO_OPPOSITE, 127);
-delay(2000);
-delay(2000);
+veer(true, 50);
+delay(5000);
+sharpturn(false, 127);
+delay(2500);
+stop;
 }
 
 void straightRun(int speed){
@@ -48,4 +43,8 @@ else rlink.command(MOTOR_2_GO, 127-speed);
 void sharpturn(bool left, int speed){
 if (left) rlink.command(BOTH_MOTORS_GO_OPPOSITE, speed);
 else rlink.command(BOTH_MOTORS_GO_OPPOSITE, 128+speed);
+}
+
+void stopMoving{
+rlink.command(BOTH_MOTORS_GO_SAME, 0);
 }
