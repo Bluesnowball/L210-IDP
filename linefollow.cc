c9f97aec97e5aec3f9a32be647c250ae69d9cb88
lv272 <lv272@cam.ac.uk>
2014-02-22 12:42:05 +0000

    Update linefollow.cc
    Added a simple switch-case statement for the sensor readings

--- linefollow.cc
+++ linefollow.cc
@@ -4,8 +4,15 @@ using namespace std;
 #include <robot_link.h>
 #include <robot_delay.h>
 #include <stopwatch.h>
+#include <header.h>
 #define ROBOT_NUM 10 // The id number (see below)
+#define LEFT 1
+#define RIGHT 0
+#define MAXSPEED 126
 robot_link rlink; // datatype for the robot link
+
+bool junct;
+int junct_count;
 
 
 int main ()
@@ -21,4 +28,68 @@ int main ()
 }
 
 
+}
+
+void decide(int sensors) {
+	static int prev = sensors; // holds the previous position of the line follower
+	switch (sensors) {
+		case 8*0+4*1+2*1+1*0:
+		straightRun(MAXSPEED);
+		prev = sensors;
+		break;
+		
+		case 8*0+4*0+2*0+1*0:
+		stop();
+		cout << "Lost the line" << endl;
+		decide(prev);
+		break;
+		
+		case 8*0+4*0+2*0+1*1:
+		veer(LEFT, MAXSPEED);
+		prev = sensors;
+		break;
+		
+		case (8*0+4*0+2*1+1*0 || 8*0+4*1+2*0+1*0 || 8*0+4*1+2*0+1*1 || 8*1+4*0+2*0+1*1 || 8*1+4*1+2*1+1*0 || 8*1+4*0+2*1+1*1 || 8*1+4*1+2*0+1*1):
+		cout << "wtf?" << endl;
+		decide(prev);
+		break;
+		
+		case 8*0+4*0+2*1+1*1:
+		veer(LEFT, 50);
+		prev = sensors;
+		break;
+		
+		case 8*0+4*1+2*1+1*1:
+		veer(RIGHT, 50);
+		prev = sensors;
+		break;
+		
+		case 8*0+4*1+2*1+1*1:
+		swivel(RIGHT, 50);
+		prev = sensors;
+		break;
+		
+		case 8*1+4*0+2*0+1*0:
+		veer(RIGHT, MAXSPEED);
+		prev = sensors;
+		break;
+		
+		case 8*1+4*1+2*0+1*0:
+		veer(RIGHT, 50);
+		prev = sensors;
+		break;
+		
+		case 8*1+4*1+2*1+1*0:
+		swivel(RIGHT, 50);
+		prev = sensors;
+		break;
+		
+		case 8*1+4*1+2*1+1*1:
+		cout << "Junction detected!"
+		junct = true;
+		junct_count++;
+		// insert pathfinding code here
+		prev = sensors;
+		break;
+	}
 }
