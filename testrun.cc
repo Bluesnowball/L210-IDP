a30574f9c52f231ba5fd8e948e281fb202abdc4b
Bluesnowball <bluesnowball1@googlemail.com>
2014-02-24 12:40:24 +0000

    Update testrun.cc
    Results of testing and changes to testing methods.
    Movement functions made to actually be useful.

--- testrun.cc
+++ testrun.cc
@@ -4,8 +4,11 @@ using namespace std;
 #include <robot_link.h>
 #include <robot_delay.h>
 #define ROBOT_NUM 10 // The id number (see below)
+#define MAXSPEED 127
+#define LEFT 0
+#define RIGHT 1
 robot_link rlink; // datatype for the robot link
-void straightRun(int speed), reverse(int speed), veer(bool left, int speed), swivel(bool left, int speed), stop();
+void straightRun(int speed), reverse(int speed), veer(bool right, int speed), swivel(bool right, int speed), stop();
 
 int main ()
 { // data from microprocessor
@@ -14,38 +17,94 @@ if (!rlink.initialise (ROBOT_NUM)) { // setup the link
   rlink.print_errs(" ");
   return -1; // error, finish
 }
+int go=0;
+while (go==0){
+cin>>go;
+}
+switch (go) {
+	case 1:
+	straightRun(MAXSPEED);
+	delay(7000);
+	break;
+	
+	case 2:
+	reverse(MAXSPEED);
+	delay(7000);
+	break;
 
-straightRun(127);
-delay(3000);
-float stat = rlink.request (STATUS);
-cout<<stat;
-veer(true, 50);
-delay(5000);
-sharpturn(false, 127);
-delay(2500);
+	case 3:
+	veer(LEFT,50);
+	delay(500);
+	break;
+	
+	case 4:
+	veer(LEFT, MAXSPEED);
+	delay(2500);
+	break;
+	
+	case 5:
+	veer(RIGHT,MAXSPEED);
+	delay(2500);
+	break;
+	
+	case 6:
+	swivel(LEFT,MAXSPEED);
+	delay(1500);
+	break;
+	
+	case 7:
+	swivel(RIGHT,MAXSPEED);
+	delay(3000);
+	break;
+	
+	case 8:
+	swivel(RIGHT,50);
+	delay(3000);
+	break;
+	
+	case 9:
+	veer(RIGHT,MAXSPEED*0.9);
+	delay(3000);
+	break;
+	
+	case 10:
+	//rlink.command(MOTOR_4_GO, MAXSPEED);
+	rlink.command(MOTOR_3_GO, MAXSPEED);
+	delay(5000);
+	break;
+}
+//float stat = rlink.request (STATUS);
+//cout<<stat;
 stop();
 }
 
 void straightRun(int speed){
 rlink.command (RAMP_TIME, 255);
-rlink.command(BOTH_MOTORS_GO_SAME, speed);
+rlink.command(BOTH_MOTORS_GO_OPPOSITE, 128+speed);
 }
 
 void reverse(int speed){
 rlink.command (RAMP_TIME, 255);
-rlink.command(BOTH_MOTORS_GO_SAME, 128+speed);
+rlink.command(BOTH_MOTORS_GO_OPPOSITE, speed);
 }
 
-void veer(bool left, int speed){
-if (left) rlink.command(MOTOR_2_GO, 127-speed);
-else rlink.command(MOTOR_2_GO, 127-speed);
+void veer(bool right, int speed){
+if (right) {
+	 rlink.command(MOTOR_1_GO, 128+MAXSPEED);
+	 rlink.command(MOTOR_2_GO, MAXSPEED-speed);
+ }
+else {
+	 rlink.command(MOTOR_1_GO, 255-speed);
+	 rlink.command(MOTOR_2_GO, MAXSPEED);
+ }
 }
 
-void swivel(bool left, int speed){
-if (left) rlink.command(BOTH_MOTORS_GO_OPPOSITE, speed);
-else rlink.command(BOTH_MOTORS_GO_OPPOSITE, 128+speed);
+void swivel(bool right, int speed){
+if (right) rlink.command(BOTH_MOTORS_GO_SAME, 128+speed);
+else rlink.command(BOTH_MOTORS_GO_SAME, speed);
 }
 
 void stop(){
 rlink.command(BOTH_MOTORS_GO_SAME, 0);
 }
+
