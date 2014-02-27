de3a6b3bf6bdb0d24e10ada04f87e1d14d5e550a
lv272 <lv272@cam.ac.uk>
2014-02-27 11:02:09 +0000

    Update header.h
    Added function prototypes and FORWARD define

--- header.h
+++ header.h
@@ -5,13 +5,13 @@
 #include <delay.h>
 #include <math.h>
 #include <stopwatch.h>
+#include <vector>
 using namespace std;
 
-#define ROBOT_NUM 10  // connects to the right robot
+#define ROBOT_NUM 11
 #define LEFT 0
 #define RIGHT 1
 #define FORWARD 2
-#define BACK 3
 #define MAXSPEED 127
 #define REVERSE 255
 
@@ -21,4 +21,6 @@ void veer(bool right, int speed);
 void swivel(bool right, int speed);
 void stop();
 void tests(int go);
-void decide (int sensors);  // line following
+void decide (int sensors);
+void Crossroads(int junct_count);
+int readLF();
