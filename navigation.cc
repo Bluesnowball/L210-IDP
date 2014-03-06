339b61d438e2dc1349cf1767956f77b701a9966a
Bluesnowball <bluesnowball1@googlemail.com>
2014-03-06 14:44:50 +0000

    Update navigation.cc
    Added the results of my very boring investigation. Finally

--- navigation.cc
+++ navigation.cc
@@ -17,37 +17,117 @@ int D3Start[2][9]={{SPINBACK,RIGHT,FORWARD,RIGHT,RIGHT,FORWARD,FORWARD,LEFT,FORW
 
 
 NavInstructions MarcoPolo(int Stype){ 
-    NavInstructions Result; /*
-    switch (Stype){
+    NavInstructions Result;
+    vector<int> tempvector1,tempvector2,tempvector3;
+    bool justpick=false //made true if just picked up, will need to actually be made working, currently just to show idea
+    if (justpick){
+    if switch (Stype){
     case 0: cout<<"No shirt, unknown location"<<endl;
       break;
     case 1: cout<<"Shirt type D1"<<endl;
       Result.name="D1";
-      Result.Instruct+=C1D1j[1]+jD1[1]+D1C1[1];
-      Result.Loc+=     C1D1j[2]+jD1[2]+D1C1[2];
+      tempvector1.assign(C1D1j[0], C1D1j[0]+(sizeof(C1D1j[0])/sizeof(C1D1j[0][0])));
+     Result.Instruct.insert(Result.Instruct.end(), tempVector1.begin(), tempVector1.end());
+     tempvector2.assign(C1D1j[1], C1D1j[1]+(sizeof(C1D1j[0])/sizeof(C1D1j[0][0])));
+          Result.Loc.insert(Result.Loc.end(), tempVector2.begin(), tempVector2.end());
+     
+     tempvector1.assign(jD1[0], jD1[0]+(sizeof(jD1[0])/sizeof(jD1[0][0])));
+     Result.Instruct.insert(Result.Instruct.end(), tempVector1.begin(), tempVector1.end());
+     tempvector2.assign(jD1[1], jD1[1]+(sizeof(jD1[0])/sizeof(jD1[0][0])));
+             Result.Loc.insert(Result.Loc.end(), tempVector2.begin(), tempVector2.end());
+     
+     tempvector1.assign(D1C1[0], D1C1[0]+(sizeof(D1C1[0])/sizeof(D1C1[0][0])));
+     Result.Instruct.insert(Result.Instruct.end(), tempVector1.begin(), tempVector1.end());
+     tempvector2.assign(D1C1[1], D1C1[1]+(sizeof(D1C1[0])/sizeof(D1C1[0][0])));
+     Result.Loc.insert(Result.Loc.end(), tempVector2.begin(), tempVector2.end());
+     
+     /* Result.Instruct+=C1D1j[1]+jD1[1]+D1C1[1];
+      Result.Loc+=     C1D1j[2]+jD1[2]+D1C1[2]; */
       Result.Indicator=0;
       break;
     case 2: cout<<"Shirt type D2"<<endl;
       Result.name="D2";
-      Result.Instruct+=C1D1j[1]+D1D2j[1]+D2jD3[1]+D2C1[1];
-      Result.Loc+=     C1D1j[2]+D1D2j[2]+D2[2]+D2C1[2];
+     tempvector1.assign(C1D1j[0], C1D1j[0]+(sizeof(C1D1j[0])/sizeof(C1D1j[0][0])));
+     Result.Instruct.insert(Result.Instruct.end(), tempVector1.begin(), tempVector1.end());
+     tempvector2.assign(C1D1j[1], C1D1j[1]+(sizeof(C1D1j[0])/sizeof(C1D1j[0][0])));
+     Result.Loc.insert(Result.Loc.end(), tempVector2.begin(), tempVector2.end());
+     
+           tempvector1.assign(D1jD2j[0], D1jD2j[0]+(sizeof(D1jD2j[0])/sizeof(D1jD2j[0][0])));
+     Result.Instruct.insert(Result.Instruct.end(), tempVector1.begin(), tempVector1.end());
+     tempvector2.assign(D1jD2j[1], D1jD2j[1]+(sizeof(D1jD2j[0])/sizeof(D1jD2j[0][0])));
+     Result.Loc.insert(Result.Loc.end(), tempVector2.begin(), tempVector2.end());
+     
+     tempvector1.assign(jD2[0], jD2[0]+(sizeof(jD2[0])/sizeof(jD2[0][0])));
+     Result.Instruct.insert(Result.Instruct.end(), tempVector1.begin(), tempVector1.end());
+     tempvector2.assign(jD2[1], jD2[1]+(sizeof(jD2[0])/sizeof(jD2[0][0])));
+     Result.Loc.insert(Result.Loc.end(), tempVector2.begin(), tempVector2.end());
+     
+          tempvector1.assign(D2C1[0], D2C1[0]+(sizeof(D2C1[0])/sizeof(D2C1[0][0])));
+     Result.Instruct.insert(Result.Instruct.end(), tempVector1.begin(), tempVector1.end());
+      tempvector2.assign(D2C1[1], D2C1[1]+(sizeof(D2C1[0])/sizeof(D2C1[0][0])));
+     Result.Loc.insert(Result.Loc.end(), tempVector2.begin(), tempVector2.end());
+     
+     /* Result.Instruct+=C1D1j[1]+D1D2j[1]+D2jD3[1]+D2C1[1];
+      Result.Loc+=     C1D1j[2]+D1D2j[2]+D2[2]+D2C1[2];*/
       Result.Indicator=0;
       break;
     case 3:
      Result.name="D3";
+      tempvector1.assign(C1D1j[0], C1D1j[0]+(sizeof(C1D1j[0])/sizeof(C1D1j[0][0])));
+     Result.Instruct.insert(Result.Instruct.end(), tempVector1.begin(), tempVector1.end());
+     tempvector2.assign(C1D1j[1], C1D1j[1]+(sizeof(C1D1j[0])/sizeof(C1D1j[0][0])));
+     Result.Loc.insert(Result.Loc.end(), tempVector2.begin(), tempVector2.end());
+     
+           tempvector1.assign(D1jD2j[0], D1jD2j[0]+(sizeof(D1jD2j[0])/sizeof(D1jD2j[0][0])));
+     Result.Instruct.insert(Result.Instruct.end(), tempVector1.begin(), tempVector1.end());
+      tempvector2.assign(D1jD2j[1], D1jD2j[1]+(sizeof(D1jD2j[0])/sizeof(D1jD2j[0][0])));
+     Result.Loc.insert(Result.Loc.end(), tempVector2.begin(), tempVector2.end());
+     
+     tempvector1.assign(D2jD3j[0], D2jD3j[0]+(sizeof(D2jD3j[0])/sizeof(D2jD3j[0][0])));
+     Result.Instruct.insert(Result.Instruct.end(), tempVector1.begin(), tempVector1.end());
+     tempvector2.assign(D2jD3j[1], D2jD3j[1]+(sizeof(D2jD3j[0])/sizeof(D2jD3j[0][0])));
+     Result.Loc.insert(Result.Loc.end(), tempVector2.begin(), tempVector2.end());
+     
+     
+          tempvector1.assign(jD3[0], jD3[0]+(sizeof(jD3[0])/sizeof(jD3[0][0])));
+     Result.Instruct.insert(Result.Instruct.end(), tempVector1.begin(), tempVector1.end());
+    tempvector2.assign(D2jD3j[1], D2jD3j[1]+(sizeof(D2jD3j[0])/sizeof(D2jD3j[0][0]))); 
+     Result.Loc.insert(Result.Loc.end(), tempVector2.begin(), tempVector2.end());
+     
+     
+     tempvector1.assign(D3C1[0], D3C1[0]+(sizeof(D3C1[0])/sizeof(D3C1[0][0])));
+     Result.Instruct.insert(Result.Instruct.end(), tempVector1.begin(), tempVector1.end());
+     tempvector2.assign(D2jD3j[1], D2jD3j[0]+(sizeof(D2jD3j[0])/sizeof(D2jD3j[0][0])));
+     Result.Loc.insert(Result.Loc.end(), tempVector2.begin(), tempVector2.end());
+     
+     
+     /*
       Result.Instruct+=C1D1j[1]+D1jD2j[1]+D2jD3j[1]+jD3[1]+D3C1;
-      Result.Loc+=     C1D1j[2]+D1jD2j[2]+D2jD3j[2]+jD3[2]+D3C1;
+      Result.Loc+=     C1D1j[2]+D1jD2j[2]+D2jD3j[2]+jD3[2]+D3C1;*/
       Result.Indicator=0;
       break;
     case 4: cout<<"Unknown shirt type, going to D1"<<endl;
-      Result.name="D1";
-      Result.Instruct+=C1D1j[1]+D1j[1]+D1C1[1];
-      Result.Loc+=     C1D1j[2]+D1j[2]+D1C1[2];
+      Result.name="D1"//copy of D1
+      tempvector1.assign(C1D1j[0], C1D1j[0]+(sizeof(C1D1j[0])/sizeof(C1D1j[0][0])));
+     Result.Instruct.insert(Result.Instruct.end(), tempVector1.begin(), tempVector1.end());
+     tempvector2.assign(C1D1j[1], C1D1j[1]+(sizeof(C1D1j[0])/sizeof(C1D1j[0][0])));
+          Result.Loc.insert(Result.Loc.end(), tempVector2.begin(), tempVector2.end());
+     
+     tempvector1.assign(jD1[0], jD1[0]+(sizeof(jD1[0])/sizeof(jD1[0][0])));
+     Result.Instruct.insert(Result.Instruct.end(), tempVector1.begin(), tempVector1.end());
+     tempvector2.assign(jD1[1], jD1[1]+(sizeof(jD1[0])/sizeof(jD1[0][0])));
+             Result.Loc.insert(Result.Loc.end(), tempVector2.begin(), tempVector2.end());
+     
+     tempvector1.assign(D1C1[0], D1C1[0]+(sizeof(D1C1[0])/sizeof(D1C1[0][0])));
+     Result.Instruct.insert(Result.Instruct.end(), tempVector1.begin(), tempVector1.end());
+     tempvector2.assign(D1C1[1], D1C1[1]+(sizeof(D1C1[0])/sizeof(D1C1[0][0])));
+     Result.Loc.insert(Result.Loc.end(), tempVector2.begin(), tempVector2.end());
+     /* Result.Instruct+=C1D1j[1]+D1j[1]+D1C1[1];
+      Result.Loc+=     C1D1j[2]+D1j[2]+D1C1[2];*/
       Result.Indicator=0;
       break;
-    } */
-    Result.name="D1";
-    Result.Indicator=0;
+    } 
+    }
     return Result; 
     }
 
