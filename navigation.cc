e01d2179a204f4ab80381c133da482c73edb0409
lv272 <lv272@cam.ac.uk>
2014-03-16 15:12:02 +0000

    Update navigation.cc

--- navigation.cc
+++ navigation.cc
@@ -1,21 +1,21 @@
 #include "header.h"
 
-int StarttoC1[2][4]={{FORWARD,RIGHT,RIGHT,PAUSE},{START,ENDOFTERM,EIETL,INGLIS}}; //Placeholders, also showing how initialization should work for these.
+int StarttoC1[2][4]={{FORWARD,RIGHT,RIGHT,PAUSE},{START,ENDOFTERM,EIETL,INGLIS}}; 
 int StarttoC2[2][5]={{FORWARD,RIGHT,FORWARD,RIGHT,PAUSE},{START,ENDOFTERM,EIETL,DPO,BAKER}};
-int C1D1[2][5]={{SPINBACK,RIGHT, RIGHT, LEFT, PAUSE},{INGLIS,EIETL,DPO,BAKER,CEBON}}; //Notation: j takes it to junction before destination,
+int C1D1[2][3]={{LEFTSPINBACK, FORWARD, PAUSE},{INGLIS,BAKER,CEBON}}; 
 //int C2D1j[2][1]={{RIGHT},{0}};
-int C1D2[2][6]={{SPINBACK,RIGHT,FORWARD,LEFT,LEFT,LEFTPAUSE},{INGLIS,EIETL,DPO,WORKSHOP,K2,SUTCLIFFE}}; //Dont have competition map, so cant be specific
+int C1D2[2][6]={{LEFTSPINBACK,FORWARD,LEFT,FORWARD,LEFT,LEFTPAUSE},{INGLIS,EIETL,DPO,WORKSHOP,K2,SUTCLIFFE}}; 
 //int C2D2[2][1]={{FORWARD},{0}};
-int C1D3[2][1]={{SPINBACK,RIGHT,FORWARD,LEFT,LEFT,FORWARD,RIGHTPAUSE},{INGLIS,EIETL,DPO,WORKSHOP,K2,MATLAB,VENKATARAMANAN}}; 
+int C1D3[2][7]={{LEFTSPINBACK,FORWARD,LEFT,FORWARD,LEFT,FORWARD,RIGHTPAUSE},{INGLIS,EIETL,DPO,WORKSHOP,K2,MATLAB,VENKATARAMANAN}}; 
 //int C2D3[2][1]={{LEFT},{0}};
 int D1C1[2][5]={{SPINBACK,RIGHT,LEFT, LEFT,PAUSE},{CEBON,BAKER,DPO,EIETL,INGLIS}};
-int D2C1[2][7]={{SPINBACK,LEFT,RIGHT,RIGHT,FORWARD,LEFT,FORWARD},{0,0,0,0,0,0,0}};//SPINBACK DOES NOT WORK HERE
-int D3C1[2][8]={{SPINBACK,RIGHT,FORWARD,RIGHT,RIGHT,FORWARD,LEFT,FORWARD},{0,0,0,0,0,0,0,0}};//OR HERE ADDED NAME TO ENUM FOR THEM THOUGH, THEY ARE THUS LEFT FOR NOW
-int D1Start[2][8]={{SPINBACK,RIGHT,LEFT,FORWARD,LEFT,FOWARD,FORWARD,STOP},{CEBON,BAKER,DPO,EIETL,ENDOFTERM,20,START}};
-int D2Start[2][7]={{SPINBACK,LEFT,LEFT,FORWARD,FORWARD,LEFT,FORWARD},{0,0,0,0,0,0,0}};
-int D3Start[2][9]={{SPINBACK,RIGHT,FORWARD,RIGHT,RIGHT,FORWARD,FORWARD,LEFT,FORWARD},{0,0,0,0,0,0,0,0,0}};
-int testinstr[2][9]={{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0}};
-//plan b is to have single arreays per journey, not concatenating them, overall probs fewer lines.
+int D2C1[2][7]={{LEFTSPINBACK,RIGHT,RIGHT,RIGHT,FORWARD,LEFT,PAUSE},{SUTCLIFFE,0,0,0,0,EIETL,INGLIS}};
+int D3C1[2][7]={{RIGHTSPINBACK,FORWARD,RIGHT,RIGHT,FORWARD,LEFT,PAUSE},{VENKATARAMANAN,0,0,0,0,EIETL,INGLIS}};
+int D1Start[2][8]={{SPINBACK,RIGHT,LEFT,FORWARD,LEFT,FORWARD,FORWARD,STOP},{CEBON,BAKER,DPO,EIETL,ENDOFTERM,20,44,START}};
+int D2Start[2][8]={{LEFTSPINBACK,LEFT,LEFT,FORWARD,FORWARD,LEFT,FORWARD,STOP},{SUTCLIFFE,0,0,0,0,0,0,START}};
+int D3Start[2][10]={{SPINBACK,RIGHT,FORWARD,RIGHT,RIGHT,FORWARD,FORWARD,LEFT,FORWARD,STOP},{VENKATARAMANAN,0,0,0,0,0,0,0,0,START}};
+int spinC1[2][4]={{SPINBACK,FORWARD,SPINBACK,PAUSE},{INGLIS,EIETL,EIETL,INGLIS}};
+int C1start[2][5]={{SPINBACK,LEFT,LEFT,FORWARD,PAUSE},{INGLIS,EIETL,EIETL,INGLIS}};
 
 NavInstructions MarcoPolo(int Stype,bool justpick,int loc){
     NavInstructions Result;
@@ -25,13 +25,19 @@ NavInstructions MarcoPolo(int Stype,bool justpick,int loc){
     //Result.name="Testrig";
      // tempvector1.assign(testinstr[0], testinstr[0]+(sizeof(testinstr[0])/sizeof(testinstr[0][0])));
      //Result.Instruct.insert(Result.Instruct.end(), tempvector1.begin(), tempvector1.end());
-   // bool justpick=false; //made true if just picked up, will need to actually be made working, currently just to show idea
     if (justpick){
 		cout<<"JustPicked"<<endl;
     switch (Stype){
-    case 0: cout<<"No shirt, unknown location"<<endl;
+    case Lingerie: cout<<"No shirt, unknown location"<<endl;
+          tempvector1.assign(spinC1[0], spinC1[0]+(sizeof(spinC1[0])/sizeof(spinC1[0][0])));
+     Result.Instruct.insert(Result.Instruct.end(), tempvector1.begin(), tempvector1.end());
+     tempvector2.assign(spinC1[1], spinC1[1]+(sizeof(spinC1[0])/sizeof(spinC1[0][0])));
+          Result.Loc.insert(Result.Loc.end(), tempvector2.begin(), tempvector2.end());
+          
+          
+      Result.Indicator = 0;
       break;
-    case 1: cout<<"Shirt type D1"<<endl;
+    case Green: cout<<"Shirt type D1"<<endl;
       Result.name="D1";
       tempvector1.assign(C1D1[0], C1D1[0]+(sizeof(C1D1[0])/sizeof(C1D1[0][0])));
      Result.Instruct.insert(Result.Instruct.end(), tempvector1.begin(), tempvector1.end());
@@ -41,7 +47,7 @@ NavInstructions MarcoPolo(int Stype,bool justpick,int loc){
    
       Result.Indicator=0;
       break;
-    case 2: cout<<"Shirt type D2"<<endl;
+    case Yellow: cout<<"Shirt type D2"<<endl;
       Result.name="D2";
      tempvector1.assign(C1D2[0], C1D2[0]+(sizeof(C1D2[0])/sizeof(C1D2[0][0])));
      Result.Instruct.insert(Result.Instruct.end(), tempvector1.begin(), tempvector1.end());
@@ -53,7 +59,7 @@ NavInstructions MarcoPolo(int Stype,bool justpick,int loc){
 Result.Loc+= C1D1j[2]+D1D2j[2]+D2[2]+D2C1[2];*/
       Result.Indicator=0;
       break;
-    case 3:
+    case Invshroom:
      Result.name="D3";
       tempvector1.assign(C1D3[0], C1D3[0]+(sizeof(C1D3[0])/sizeof(C1D3[0][0])));
      Result.Instruct.insert(Result.Instruct.end(), tempvector1.begin(), tempvector1.end());
@@ -63,7 +69,7 @@ Result.Loc+= C1D1j[2]+D1D2j[2]+D2[2]+D2C1[2];*/
    
       Result.Indicator=0;
       break;
-    case 4: cout<<"Unknown shirt type, going to D1"<<endl;
+    case Burkha: cout<<"Unknown shirt type, going to D1"<<endl;
 Result.name="D1"; //copy of D1
      Result.name="D1";
       tempvector1.assign(C1D1[0], C1D1[0]+(sizeof(C1D1[0])/sizeof(C1D1[0][0])));
@@ -82,7 +88,7 @@ Result.name="D1"; //copy of D1
           Result.Loc.insert(Result.Loc.end(), tempvector2.begin(), tempvector2.end());	
           Result.Indicator=0;
 	}
-	else if (loc==CEBON){
+	else if (loc==CEBON || loc == 25){
 		//add time/shirt check here
 		cout<<"Going back for more Mech"<<endl;
 	tempvector1.assign(D1C1[0], D1C1[0]+(sizeof(D1C1[0])/sizeof(D1C1[0][0])));
@@ -92,7 +98,7 @@ Result.name="D1"; //copy of D1
           Result.Indicator=0;	
 		
 	}
-	else if (loc==SUTCLIFFE){
+	else if (loc==SUTCLIFFE || loc == 1077627992){
 			cout<<"Going back for more Mat"<<endl;
 	tempvector1.assign(D2C1[0], D2C1[0]+(sizeof(D2C1[0])/sizeof(D2C1[0][0])));
      Result.Instruct.insert(Result.Instruct.end(), tempvector1.begin(), tempvector1.end());
@@ -108,11 +114,18 @@ Result.name="D1"; //copy of D1
           Result.Loc.insert(Result.Loc.end(), tempvector2.begin(), tempvector2.end());	
           Result.Indicator=0;
 	}
+	 else if (loc==INGLIS){
+	 cout<<"Going home"<<endl;
+     tempvector1.assign(spinC1[0], spinC1[0]+(sizeof(spinC1[0])/sizeof(spinC1[0][0])));
+     Result.Instruct.insert(Result.Instruct.end(), tempvector1.begin(), tempvector1.end());
+     tempvector2.assign(spinC1[1], spinC1[1]+(sizeof(spinC1[0])/sizeof(spinC1[0][0])));
+          Result.Loc.insert(Result.Loc.end(), tempvector2.begin(), tempvector2.end());
+	  }
 	for(unsigned int i=0;i<Result.Instruct.size();i++){
 cout<<Result.Instruct[i]<<endl;
  }
     return Result;
-    }
+}
 
 
 int Lost(int prev){
