2607425aac0bcfff6b2bf32be167f2d79f4f933d
lv272 <lv272@cam.ac.uk>
2014-03-16 15:11:43 +0000

    Update linefollow.cc

--- linefollow.cc
+++ linefollow.cc
@@ -1,39 +1,31 @@
 #include "header.h"
 
-bool pickup = true;
-//unsigned int junct_count = 0;
-//int directions[10] = {FORWARD, RIGHT, RIGHT, PAUSE, SPINBACK, RIGHT, RIGHT, LEFT, PAUSE, STOP};
-//int direcs1[5] = {SPINBACK, FORWARD, LEFT, PAUSE, STOP};
+bool supervision = false;
 
 stopwatch watch;
+int Stype;
 
 void decide(int sensors) {	// line following
 int temp=0;
-	if (ShiningPath.Indicator >= ShiningPath.Instruct.size()) {
-		temp=ShiningPath.Loc[ShiningPath.Indicator];
-		ShiningPath = MarcoPolo(Green, !pickup, temp);
+if (ShiningPath.Indicator >= ShiningPath.Instruct.size() && ShiningPath.Instruct.size()!= 0) {
+		temp=ShiningPath.Loc[ShiningPath.Indicator-1];
+		cout << "location index " << temp << endl;
+		ShiningPath = MarcoPolo(Stype, supervision, temp);
 		ShiningPath.Indicator = 0;
 	}
-if ((ShiningPath.Instruct[ShiningPath.Indicator] == SPINBACK || ShiningPath.Instruct[ShiningPath.Indicator] == STOP) && watch.read() == 0){ 
+if ((ShiningPath.Instruct[ShiningPath.Indicator] == SPINBACK || ShiningPath.Instruct[ShiningPath.Indicator] == STOP || ShiningPath.Instruct[ShiningPath.Indicator] == LEFTSPINBACK || ShiningPath.Instruct[ShiningPath.Indicator] == RIGHTSPINBACK )){ 
 	ShiningPath.Indicator++;
+	cout << "i got " << ShiningPath.Indicator << " instructions" << endl;
 	Crossroads(ShiningPath.Indicator-1);
-} 
-// makes sure only the sensors are read - bits 0-3
-//cout << sensors << endl;
-float Kp = 24;
-float Kd = 16;
-float Ki = 0.09;	// proportional, integral and derivative constants - need testing
+}
+const int Kp = 24;
+const int Kd = 16;
+const float Ki = 0.3;	// proportional, integral and derivative constants - need testing
 static int prev = sensors; // holds the previous position of the line follower
-if (prev == 15) Kd = 1;
+//if (prev == 15) Kd = 3;
 static float I = 0; // initialise integral control
 static int olderror = 0; // initialise proportional control
 int errorterm = 0;
-/*
-if (sensors >= 8) errorterm = errorterm - 1;
-if ((sensors%8-sensors%4-sensors%2) == 4) errorterm = errorterm -1;
-if ((sensors%4-sensors%2) == 2) {errorterm = errorterm + 1;}
-if ((sensors%2) == 1) {errorterm = errorterm + 1;}
-*/ 
 switch(sensors) {
 	case 1:
 	errorterm = 3;
@@ -71,93 +63,170 @@ switch(sensors) {
 	prev = sensors;
 	errorterm = 0;
 	break;
-	
-	default:
-	break;
 }
-float P = Kp*errorterm;
+int P = Kp*errorterm;
 I = I + errorterm*Ki;
-float D = (errorterm - olderror)*Kd;
+int D = (errorterm - olderror)*Kd;
 int control = P+I+D;				// implements PID control		
 bool direction = RIGHT;
 if (control < 0) {
-	control = 0-control;
+	control = -control;
 	direction = LEFT;
 }
-if (sensors == 0) control = 0;
+//if (sensors == 0) control = 0;
 if (control > MAXSPEED*THROTTLE) control = MAXSPEED*THROTTLE;
 olderror = errorterm;	// avoids messing up derivative control at junctions
 veer(direction, control);
 if (sensors != 0) prev = sensors;		// updates last position unless it's lost
+//cout << "elapsed " << watch.read() << endl;
 }
 
 void Crossroads(int junctcount) {
+	int dist, threshold;
 	int dir = ShiningPath.Instruct[junctcount];
+	if (junctcount == MAXSPEED) dir = FORWARD;		// kludge for unintended uses
+	
 	int linereps = 0;
 	int input = 8*1+4*1+2*1+1*1;
 	bool line1 = false;
-		if (dir == STOP) {
-			stop();
-			delay(100000);
-		}
-		if (dir == PAUSE) {
+	switch (dir) {
+		case STOP:
+		stop();
+		delay(100000);
+		break;
+		
+		case PAUSE:
+		switch (ShiningPath.Loc[junctcount]){
+			case INGLIS:
+			case BAKER:
+			threshold = 82;
+			break;
 			
-			watch.start();
-			while (watch.read() < 650) {
-				Crossroads(0);
-				Advance();
-			}
-			watch.stop();
-			stop();
-			delay(1000);
-			Manipulate(pickup);
-			if (pickup == true) {
-			int shirt = Identify();
-			cout << shirt << endl;
-			}
-			pickup = !pickup;
+			case CEBON:
+			threshold = 155;
+			break;
 		}
-		if (dir == SPINBACK) {
-				stop();
-				straightRun(REVERSE);
-				delay(100/THROTTLE);
-				swivel(dir, 0.6*MAXSPEED);
-				delay(600);
-				input = readLF();
-				cout << "hi" << endl;
-			while (linereps < 8 || line1 == false) {
-				swivel(dir, 0.6*MAXSPEED);
-				delay(33);
-				input = readLF();
-				if (input != 0 && input != 15) linereps++;
-				else linereps = 0;
-				if (linereps == 14 && line1 == false) {
+		dist = readDistance();
+		while (dist < threshold) {
+			Crossroads(MAXSPEED);				// kludge to make it go ahead
+			Advance();
+			dist = readDistance();
+		}
+		stop();
+		Manipulate(!supervision);
+		if (supervision == false) {
+			Stype = Identify();
+			cout << Stype << endl;
+		}
+		supervision = !supervision;
+		break;
+		
+		case SPINBACK:
+		stop();
+		straightRun(REVERSE);
+		delay(150/THROTTLE);
+		swivel(LEFT, 0.8*MAXSPEED);
+		delay(600);
+		input = readLF();
+		cout << "hi" << endl;
+		while (linereps < 8 || line1 == false) {
+			swivel(LEFT, 0.8*MAXSPEED);
+			delay(35);
+			input = readLF();
+			if (input != 0 && input != 15) linereps++;
+			else linereps = 0;
+			if (linereps == 8 && line1 == false) {
 					line1 = true;
 					linereps = 0;
-				}
-				//cout << "loopy" << endl;
 			}
 		}
-		if (dir==LEFT || dir==RIGHT) {
-				straightRun(MAXSPEED*THROTTLE);
-				delay(300/THROTTLE);
-				swivel(dir, MAXSPEED*0.6);
-				delay(800);
-				input = readLF();
-				//cout << "hi" << endl;
-			while (linereps < 8) {
-				swivel(dir, 0.6*MAXSPEED);
-				delay(33);
-				input = readLF();
-				if (input != 0 && input != 15) linereps++;
-				else linereps = 0;
-				//cout << "loopy" << endl;
-			}				
+		break;
+		
+		case LEFTSPINBACK:
+		case RIGHTSPINBACK:
+		case LEFT:
+		case RIGHT:
+		if (dir != LEFTSPINBACK && dir != RIGHTSPINBACK) {
+			straightRun(MAXSPEED*THROTTLE);
+			delay(550/THROTTLE);
 		}
-		else 
-		straightRun(MAXSPEED*THROTTLE);
-		while (input == 15) {
+		else {
+			if (dir == LEFTSPINBACK) dir = LEFT;
+			else dir = RIGHT;
+		}
+		swivel(dir, MAXSPEED*0.9);
+		delay(500);
 		input = readLF();
-	}
-	decide(input);	
+		while (linereps < 8) {
+			swivel(dir, 0.9*MAXSPEED);
+			delay(35);
+			input = readLF();
+			if (input != 0 && input != 15) linereps++;
+			else linereps = 0;
+		}
+		break;
+		
+		
+		case LEFTPAUSE:
+		straightRun(MAXSPEED*THROTTLE);
+		delay(450/THROTTLE);
+		swivel(LEFT, MAXSPEED*0.8);
+		delay(800);
+		input = readLF();
+		while (linereps < 8) {
+			swivel(LEFT, 0.8*MAXSPEED);
+			delay(35);
+			input = readLF();
+			if (input != 0 && input != 15) linereps++;
+			else linereps = 0;
+		}
+		watch.start();
+		while (watch.read() < 1250) {
+			Advance();
+		}
+		stop();
+		Manipulate(!supervision);
+		if (supervision == false) {
+			Stype = Identify();
+			cout << Stype << endl;
+		}
+		watch.start();
+		while (watch.read() < 150) {
+			Advance();
+		}
+		supervision = !supervision;
+		break;
+		
+		case RIGHTPAUSE:
+		straightRun(MAXSPEED*THROTTLE);
+		delay(450/THROTTLE);
+		swivel(RIGHT, MAXSPEED*0.8);
+		delay(800);
+		input = readLF();
+		while (linereps < 8) {
+			swivel(RIGHT, 0.8*MAXSPEED);
+			delay(35);
+			input = readLF();
+			if (input != 0 && input != 15) linereps++;
+			else linereps = 0;
+		}
+		watch.start();
+		while (watch.read() < 100) {	
+			Advance();
+		}
+		stop();
+		Manipulate(!supervision);
+		if (supervision == false) {
+			Stype = Identify();
+			cout << Stype << endl;
+		}
+		supervision = !supervision;
+		break;
+				
+		case FORWARD:
+		default:
+		straightRun(MAXSPEED*THROTTLE);
+		while (input == 15) input = readLF();
+		break;				
+	}	
 }
